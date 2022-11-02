d505a14b12b11962122e7eaee13a6648e3be63bd
Edufgs <eduardo.goncal.silva@gmail.com>
2022-11-02 17:15:04 -0400

    Utilizando Angular Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
-const routes: Routes = [];
+//Aqui fica a configuração de todos as rotas
+const routes: Routes = [
+  //'' = Significa rota principal e tem que dizar o componente que vai rederizar
+  {path: '', component: FirstComponentComponent},
+  {path: 'list', component: ListRenderComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
